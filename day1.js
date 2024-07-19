fa576ae91ba68dc8a043083692f829f7b1379196
paridhi-kapoor <paridhikapoor2004@gmail.com>
2024-07-19 15:04:11 +0530

    added day1.js

---
+++ day1.js
@@ -0,0 +1,19 @@
+//day1 
+
+  //Activity 1:Variable Declaration
+
+    // task1: Declare a variable using var,assign it to a number and log the value console.
+
+    var Pincode = 302036; 
+    console.log(Pincode)  //output: 302036
+
+    //task 2:Declare a variable using let,assign it to a string and log the value console.
+
+    let city = "Jaipur" ;
+    console.log(city)  //output: Jaipur
+
+  //Activity 2:  
+  
+   // task1: Declare a variable const,assign it to a boolean value,and log the value to the console.
+    const boolconst = true;
+    console.log(boolconst)  //true
