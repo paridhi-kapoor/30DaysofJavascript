71d0d69df991a237252f72004dee038f492d673c
paridhi-kapoor <paridhikapoor2004@gmail.com>
2024-07-24 23:28:08 +0530

    added day6.js file

---
+++ day6.js
@@ -0,0 +1,21 @@
+//DAY 6: ARRAYS
+
+//ACTIVITY 1: Array creation and access
+
+  //task 1 : Create an array of numbers from 1 to 5 and log the array to console.
+   
+   const num = [1, 2, 3,4,5] ;
+   console.log("num :" , num)
+
+   //OUTPUT : num : [ 1, 2, 3, 4, 5 ]
+
+  //TASK 2 : Access the first and last element of an array and log the result to console.
+
+   firstElement = num[0] ;                        //first element of array num
+   lastElement = num[num.length -1] ;            //last element of array num
+   
+   console.log("first element is ", firstElement) ;          //OUTPUT :
+  
+   console.log("last element is ", lastElement) ;           //OUTPUT :  
+
+   
